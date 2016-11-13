fc9c2c7d71a94860169ce6fa9857045cd8a7a639
lil-works <lilian@lil-works.com>
2016-11-13 19:32:49 +0100

    some more feature

--- pro.sh
+++ pro.sh
@@ -4,5 +4,6 @@ php bin/console generate:doctrine:entities AppBundle
 php bin/console doctrine:schema:update --force
 php bin/console assets:install --symlink
 php bin/console assetic:dump
+php bin/console cache:clear
 
 
